fe0a9a6dd1034803be1bb9a7b8cb3a5e321dc15c
Tossmann <trautmann.phillip@gmx.de>
2016-12-19 15:26:37 +0100

    encryption implemented

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.io.Console;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.Dictionary;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -10,7 +13,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
 
 public class Main {
 
@@ -19,71 +25,90 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		importWordList();
 		ourInteface();
+		simpleEncryption();
 	}
 
-	private static void ourInteface() {
+	private static void ourInteface() throws Exception {
 		String input = "";
 		Scanner in = new Scanner(System.in);
-		
+
 		while (!input.equals("exit")) {
 			printOptions();
 			input = in.nextLine();
 			runOption(input);
 		}
 	}
-	
-	private static void runOption(String input) {
+
+	private static void runOption(String input) throws Exception {
 		switch (input) {
-        case "encrypt": simpleEncryption();
-                 break;
-        case "exit" : 	printExitText();
-        		break;
-        case "bruteforce": bruteForce();
-        		break;
+		case "encrypt": simpleEncryption();
+		break;
+		case "exit" : 	printExitText();
+		break;
+		case "bruteforce": bruteForce();
+		break;
 		}
 	}
-	
-	private static void simpleEncryption() {
+
+	private static void simpleEncryption() throws Exception {
 		String password = getPassword();
 		String message = getString("Please enter your message: ");
 		String cypher = encryptMessage(password,message);
 		System.out.println("Cypher: " + cypher);
 	}
-	
+
 	private static void bruteForce() {
 		String cypher = getString("Please enter the cypher-message: ");
 		System.out.println(cypher);
 	}
-	
+
 	private static String getPassword(){
 		String password = findRandomWord();
 		System.out.println("Your randomly choosed Password is: '" + password + "'");
 		return password;
 	}
-	
+
 	private static String getString(String message) {
 		System.out.println(message);
 		Scanner scanInput = new Scanner(System.in);
 		String input = scanInput.nextLine();
 		return input;
 	}
-	
-	
-	
-	private static String encryptMessage(String password, String message) {
-		return (message + password);
+
+
+
+	private static String encryptMessage(String password, String message) throws Exception {
+
+		byte[] keyBytes = new byte[16];
+		byte[] input = new byte[16];
+		byte[] cipherText;
+
+		String hexPw = toHex(password);
+		String hexMessage = toHex(message);
+		input = toByteArray(hexMessage);
+		keyBytes = toByteArray(hexPw);
+
+
+		SecretKeySpec key = new SecretKeySpec(keyBytes, "Blowfish");
+		Cipher        cipher = Cipher.getInstance("Blowfish/ECB/ISO10126Padding", "BC");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		cipherText = new byte[cipher.getOutputSize(input.length)];
+		int ctLength = cipher.update(input, 0, input.length, cipherText, 0);
+		ctLength += cipher.doFinal(cipherText, ctLength);
+
+		return Utils.toHex(cipherText);
 	}
-	
+
 	private static void printExitText() {
 		System.out.println("Bye"); 
 	}
-	
+
 	private static void printOptions() {
 		System.out.println();
 		System.out.println("what you want to do? Your options are:");
 		System.out.println("'exit' 'encrypt' 'bruteforce'");
 	}	
-	
+
 	private static void encryption(){
 
 		/*
@@ -129,10 +154,18 @@ public class Main {
 		System.out.println("plain text : " + Utils.toHex(plainText) + " bytes: " + ptLength);
 		 */
 	}
-	
+
+	public static byte[] toByteArray(String hexString){
+		return DatatypeConverter.parseHexBinary(hexString);
+	}
+
+	public static String toHex(String arg) {
+		return String.format("%040x", new BigInteger(1, arg.getBytes(/*YOUR_CHARSET?*/)));
+	}
+
 	private static void importWordList() throws IOException {
 		System.out.println("Import of Wordlists starts ...");
-		
+
 		BufferedReader br = null;
 		String sCurrentLine;
 		br = new BufferedReader(new FileReader("src/words.txt"));
@@ -140,11 +173,11 @@ public class Main {
 			dictionary.add(sCurrentLine);
 		}
 		if (br != null) br.close();
-		
+
 		System.out.println("Import of Wordlists succeded");
 		System.out.println("Dictanary has " + dictionary.size() + " entries.");
 	}
-	
+
 	public static String findRandomWord(){
 		Random rn = new Random();
 		int randomNumber = rn.nextInt(dictionary.size());
