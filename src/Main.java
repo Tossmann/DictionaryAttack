13180ff4cd34b21a27f210a9bff0c896457bf113
AndreyDelany <dielanie@freenet.de>
2016-12-19 15:12:38 +0100

    created to new classes for better structure in project

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,6 @@ public class Main {
 	private static HashSet<String> dictionary = new HashSet<>();
 
 	public static void main(String[] args) throws Exception{
-		analyzeProbabelPlainMessage("hallodusackgesicht");
 		importWordList();
 		ourInteface();
 	}
@@ -48,51 +47,15 @@ public class Main {
 	private static void simpleEncryption() {
 		String password = getPassword();
 		String message = getString("Please enter your message: ");
-		String cypher = encryptMessage(password,message);
-		
-		
+		String cypher = EnAndDecryption.encrypt(password,message);
 		System.out.println("Cypher: " + cypher);
 	}
 	
 	private static void bruteForce() {
 		String cypher = getString("Please enter the cypher-message: ");
-		
-		ArrayList probabelPlainMessages = new ArrayList();
-		
-		Iterator iterator = dictionary.iterator();
-		while (iterator.hasNext()) {
-			String probabelKey = (String) iterator.next();
-		    String probabelPlainMessage = decrypt(probabelKey,cypher);
-		    if (analyzeProbabelPlainMessage(probabelPlainMessage)) 
-		    		probabelPlainMessages.add(probabelPlainMessage);
-		}
-		
-		System.out.println(cypher);
-	}
-	
-	private static String decrypt(String key,String cypher) {
-		return  cypher; //TODO
-	}
-	
-	private static Boolean analyzeProbabelPlainMessage(String message) {
-		String firstPartOfMessage = message.substring(0, 9);
-		String[] splittetMessage;
-		for (int i = 2; i < 10; i ++) {
-			splittetMessage = firstPartOfMessage.split("(?<=\\G.{" + i + "})");
-			if (analyzeSplittetMessage(splittetMessage))
-				return true;
-		}
-		return false;
-	}
-	
-	private static Boolean analyzeSplittetMessage(String [] splittetMessage) {
-		for (String part : splittetMessage){
-		      if (dictionary.contains(part)) {
-		         return true;
-		      }
-		   }
-		return false;
-	}
+		BruteForce bruteForce = new BruteForce(dictionary);
+		ArrayList<String> answers = bruteForce.doBruteForcing(cypher);
+	}	
 	
 	private static String getPassword(){
 		String password = findRandomWord();
@@ -107,10 +70,6 @@ public class Main {
 		return input;
 	}
 	
-	private static String encryptMessage(String password, String message) {
-		return (message + password);
-	}
-	
 	private static void printExitText() {
 		System.out.println("Bye"); 
 	}
@@ -120,52 +79,6 @@ public class Main {
 		System.out.println("what you want to do? Your options are:");
 		System.out.println("'exit' 'encrypt' 'bruteforce'");
 	}	
-	
-	private static void encryption(){
-
-		/*
-
-		byte[]        input = new byte[] { 
-				0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 
-				(byte)0x88, (byte)0x99, (byte)0xaa, (byte)0xbb,
-				(byte)0xcc, (byte)0xdd, (byte)0xee, (byte)0xff };
-		byte[]        keyBytes = new byte[] { 
-				0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-				0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
-
-		SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-
-		Cipher        cipher = Cipher.getInstance("AES/ECB/NoPadding", "BC");
-
-
-		System.out.println("input text : " + Utils.toHex(input));
-
-		// encryption pass
-
-		byte[] cipherText = new byte[input.length];
-
-		cipher.init(Cipher.ENCRYPT_MODE, key);
-
-		int ctLength = cipher.update(input, 0, input.length, cipherText, 0);
-
-		ctLength += cipher.doFinal(cipherText, ctLength);
-
-		System.out.println("cipher text: " + Utils.toHex(cipherText) + " bytes: " + ctLength);
-
-		// decryption pass
-
-		byte[] plainText = new byte[ctLength];
-
-		cipher.init(Cipher.DECRYPT_MODE, key);
-
-		int ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
-
-		ptLength += cipher.doFinal(plainText, ptLength);
-
-		System.out.println("plain text : " + Utils.toHex(plainText) + " bytes: " + ptLength);
-		 */
-	}
 	
 	private static void importWordList() throws IOException {
 		System.out.println("Import of Wordlists starts ...");
